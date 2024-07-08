8629774d60064769f8eb9e8b81f98473dca4da70
cadkin <adkinscv@ornl.gov>
2024-07-08 13:55:20 -0400

    Port nix preset and toolchain from pathcompiler

---
+++ cmake/toolchains/nix.cmake
@@ -0,0 +1,71 @@
+if(NOT "$ENV{NIX_PROFILE_LOADED}")
+    message(STATUS "Setting up environment via Nix...")
+
+    find_program(NIX_EXECUTABLE nix)
+
+    if(NOT NIX_EXECUTABLE)
+        message(FATAL_ERROR "Nix executable not found, is it installed?")
+    endif()
+
+    execute_process(
+        COMMAND ${NIX_EXECUTABLE} print-dev-env ${NIX_DEVELOP_PROFILE} -L --json
+        RESULT_VARIABLE NIX_DEVELOP_RESULT
+        OUTPUT_VARIABLE NIX_DEVELOP_OUTPUT
+    )
+
+    if(${NIX_DEVELOP_RESULT})
+        message(FATAL_ERROR "Failed to setup environment, check above for details.")
+    endif()
+
+    message(STATUS "Loading environment...")
+
+    function(LoadNixPathVar)
+        string(JSON VAR_VALUE ERROR_VARIABLE JSON_ERROR GET "${NIX_DEVELOP_OUTPUT}" "variables" "PATH" "value")
+        if("${JSON_ERROR}" STREQUAL "NOTFOUND")
+            message(STATUS "Appending PATH")
+            string(APPEND ENV{PATH} ":${VAR_VALUE}")
+        endif()
+    endfunction()
+
+    function(LoadNixEnvVar LOAD_VAR)
+        string(JSON VAR_VALUE ERROR_VARIABLE JSON_ERROR GET "${NIX_DEVELOP_OUTPUT}" "variables" "${LOAD_VAR}" "value")
+        if("${JSON_ERROR}" STREQUAL "NOTFOUND")
+            message(STATUS "Setting ${LOAD_VAR}")
+            set(ENV{${LOAD_VAR}} ${VAR_VALUE})
+        endif()
+    endfunction()
+
+    function(LoadNixProgToCMakeCache LOAD_VAR CACHE_VAR)
+        string(JSON VAR_VALUE ERROR_VARIABLE JSON_ERROR GET "${NIX_DEVELOP_OUTPUT}" "variables" "${LOAD_VAR}" "value")
+        if("${JSON_ERROR}" STREQUAL "NOTFOUND")
+            message(STATUS "Setting ${CACHE_VAR} to ${VAR_VALUE}")
+            find_program(tmp NAMES "${VAR_VALUE}" NO_CACHE)
+            set(${CACHE_VAR} "${tmp}" CACHE FILEPATH "Path to ${LOAD_VAR}" FORCE)
+            message(STATUS "Found ${CACHE_VAR} at ${tmp}")
+        endif()
+    endfunction()
+
+    LoadNixPathVar()
+
+    LoadNixEnvVar("PKG_CONFIG_PATH")
+    LoadNixEnvVar("PYTHONPATH")
+    LoadNixEnvVar("CMAKE_INCLUDE_PATH")
+    LoadNixEnvVar("CMAKE_LIBRARY_PATH")
+    LoadNixEnvVar("CMAKE_PREFIX_PATH")
+    LoadNixEnvVar("QMAKE")
+    LoadNixEnvVar("QMAKEPATH")
+    LoadNixEnvVar("QTTOOLSPATH")
+    LoadNixEnvVar("QT_ADDITIONAL_PACKAGES_PREFIX_PATH")
+
+    # TODO: Make this work better - there's some weirdness when specifying the compiler from the toolchain.
+    # Really QtCreator shouldn't be specifying an empty compiler.
+    LoadNixProgToCMakeCache("CXX" CMAKE_CXX_COMPILER)
+    LoadNixProgToCMakeCache("CC"  CMAKE_C_COMPILER)
+
+    message(STATUS "Environment loaded.")
+
+    set(CMAKE_SYSTEM_NAME Linux)
+    set(CMAKE_SYSTEM_PROCESSOR x86_64)
+
+    set(ENV{NIX_PROFILE_LOADED} "1")
+endif()
--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
     description = "ORNL Slicer 2 - An advanced object slicer";
 
     inputs = {
-        nixpkgs.url = github:mdfbaam/nixpkgs/pathcompiler;
+        nixpkgs.url = github:mdfbaam/nixpkgs/pathcompiler-staging;
+        utils.url = github:numtide/flake-utils;
 
         kubazip-src = {
             url = github:kuba--/zip;
@@ -22,92 +23,112 @@
         };
     };
 
-    outputs = attrs @ { self, nixpkgs, ... }: rec {
-        system = "x86_64-linux";
+    outputs = attrs @ { self, ... }: attrs.utils.lib.eachDefaultSystem (system: rec {
+        config = rec {
+            pkgsNative = import attrs.nixpkgs {
+                inherit system;
+                config = {
+                    glibc.withLdFallbackPatch = true;
+                };
+            };
 
-        pkgs = import nixpkgs {
-            inherit system;
-            config = {
-                glibc.withLdFallbackPatch = true;
+            pkgsMinGW64 = (import attrs.nixpkgs { inherit system; }).pkgsCross.mingwW64;
+
+            llvm = pkgsNative.llvmPackages_18;
+        };
+
+        derivations = { pkgs, stdenv ? pkgs.stdenv }: with config; rec {
+            ornl = {
+                slicer2 = stdenv.mkDerivation rec {
+                    pname = "ornl-slicer2";
+                    version = "1.0-" + (builtins.substring 0 8 (if (self ? rev) then self.rev else "dirty"));
+
+                    src = self;
+
+                    buildInputs = [
+                        pkgs.cmake
+                        pkgs.pkg-config
+                        pkgs.ninja
+                        pkgs.libGL
+                        pkgs.qt5.qtbase
+                        pkgs.qt5.qtcharts
+
+                        pkgs.assimp
+                        pkgs.boost184
+                        pkgs.cgal_5
+                        pkgs.eigen
+                        pkgs.gmp
+                        pkgs.nlohmann_json
+                        pkgs.mpfr
+
+                        libs.kubazip
+                        libs.nlohmann_fifomap
+                        libs.s2clipper
+                        libs.ornltcp-sockets
+                        libs.psimpl
+                        libs.qtxlsxwriter
+                    ];
+
+                    nativeBuildInputs = [
+                        pkgsNative.breakpointHook
+                        pkgsNative.qt5.wrapQtAppsHook
+                    ];
+
+                    # Workaround for non-NixOS to find GPU drivers
+                    # TODO: add more linux distro paths as they are found or query ldconfig
+                    qtWrapperArgs = [
+                        "--suffix LD_LIBRARY_PATH : /usr/lib/x86_64-linux-gnu"
+                    ];
+
+                    # The current build system is a bit weird, so we have to do some manual copying.
+                    installPhase = ''
+                        runHook preInstall
+
+                        mkdir -p $out/bin
+                        cp ornl_slicer_2 $out/bin/slicer2
+                        cp -r templates $out/bin
+                        cp -r layerbartemplates $out/bin
+
+
+                        mkdir -p $out/share/doc
+                        cp Slicer_2_User_Guide.pdf $out/share/doc
+
+                        runHook postInstall
+                    '';
+
+                    meta.mainProgram = "slicer2";
+                };
+            };
+
+            libs = {
+                kubazip          = import contrib/nix/kubazip.nix      { inherit pkgs; source = attrs.kubazip-src; };
+                nlohmann_fifomap = import contrib/nix/fifomap.nix      { inherit pkgs; source = attrs.nlohmann-fifomap-src; };
+                ornltcp-sockets  = import contrib/nix/tcpsockets.nix   { inherit pkgs; source = "${contrib/ORNL-TCP-Sockets}"; };
+                psimpl           = import contrib/nix/psimpl.nix       { inherit pkgs; source = attrs.psimpl-src; };
+                qtxlsxwriter     = import contrib/nix/qtxlsxwriter.nix { inherit pkgs; source = attrs.qtxlsxwriter-src; };
+                # Clipper lib is a bit different since we use a version not available anywhere else.
+                # So we have to use the one from the slicer 2 repo itself.
+                s2clipper        = import contrib/nix/s2clipper.nix    { inherit pkgs; source = "${contrib/clipper}"; };
+            };
+
+            ide = {
+                qtcreator = pkgs.qtcreator.overrideAttrs (final: prev: {
+                    qtWrapperArgs = prev.qtWrapperArgs ++ pkgs.lib.optionals stdenv.isLinux [
+                        "--suffix LD_FALLBACK_PATH : /usr/lib/x86_64-linux-gnu"
+                    ];
+
+                    cmakeFlags = prev.cmakeFlags ++ [
+                        "-DBUILD_PLUGIN_CLANGFORMAT=OFF"
+                    ];
+                });
             };
         };
 
-        llvm   = pkgs.llvmPackages_18;
-        stdenv = llvm.stdenv;
+        packages = with config; rec {
+            default = ornl.slicer2;
 
-
-        packages.${system} = rec {
-            default = ornl-slicer2;
-
-            # Main Slicer 2 derivation.
-            ornl-slicer2 = stdenv.mkDerivation rec {
-                pname = "ornl-slicer2";
-                version = "1.0-" + (builtins.substring 0 8 (if (self ? rev) then self.rev else "dirty"));
-
-                src = self;
-
-                buildInputs = [
-                    pkgs.cmake
-                    pkgs.pkg-config
-                    pkgs.ninja
-                    pkgs.libGL
-                    pkgs.qt5.qtbase
-                    pkgs.qt5.qtcharts
-
-                    pkgs.assimp
-                    pkgs.boost184
-                    pkgs.cgal_5
-                    pkgs.eigen
-                    pkgs.gmp
-                    pkgs.nlohmann_json
-                    pkgs.mpfr
-
-                    kubazip
-                    nlohmann_fifomap
-                    s2clipper
-                    ornltcp-sockets
-                    psimpl
-                    qtxlsxwriter
-                ];
-
-                nativeBuildInputs = [
-                    pkgs.breakpointHook
-                    pkgs.qt5.wrapQtAppsHook
-                ];
-
-                # Workaround for non-NixOS to find GPU drivers
-                # TODO: add more linux distro paths as they are found or query ldconfig
-                qtWrapperArgs = [
-                    "--suffix LD_LIBRARY_PATH : /usr/lib/x86_64-linux-gnu"
-                ];
-
-                installPhase = ''
-                    runHook preInstall
-
-                    mkdir -p $out/bin
-                    cp ornl_slicer_2 $out/bin/slicer2
-                    cp -r templates $out/bin
-                    cp -r layerbartemplates $out/bin
-
-
-                    mkdir -p $out/share/doc
-                    cp Slicer_2_User_Guide.pdf $out/share/doc
-
-                    runHook postInstall
-                '';
-
-                meta.mainProgram = "slicer2";
-            };
-
-            # Other packages we make derivations for - i.e. not in nixpkgs already.
-            kubazip          = import contrib/nix/kubazip.nix      { inherit pkgs; source = attrs.kubazip-src; };
-            nlohmann_fifomap = import contrib/nix/fifomap.nix      { inherit pkgs; source = attrs.nlohmann-fifomap-src; };
-            ornltcp-sockets  = import contrib/nix/tcpsockets.nix   { inherit pkgs; source = "${contrib/ORNL-TCP-Sockets}"; };
-            psimpl           = import contrib/nix/psimpl.nix       { inherit pkgs; source = attrs.psimpl-src; };
-            qtxlsxwriter     = import contrib/nix/qtxlsxwriter.nix { inherit pkgs; source = attrs.qtxlsxwriter-src; };
-            # Clipper lib is a bit different since we use a version not available anywhere else.
-            # So we have to use the one from the slicer 2 repo itself.
-            s2clipper        = import contrib/nix/s2clipper.nix    { inherit pkgs; source = "${contrib/clipper}"; };
+            inherit ( derivations { pkgs = pkgsNative; stdenv = llvm.stdenv; } ) ornl libs ide;
+            windows = derivations { pkgs = pkgsMinGW64; };
         };
 
         bundlers.${system} = rec {
@@ -119,31 +140,39 @@
 
             ornl-slicer2 = {
                 type = "app";
-                program = "${self.packages.${system}.ornl-slicer2}/bin/slicer2";
+                program = "${self.packages.${system}.ornl.slicer2}/bin/slicer2";
             };
         };
 
-        devShells.${system} = rec {
-            default = ornl-slicer2-dev;
+        devShells = with config; rec {
+            default = s2-dev;
 
             # Main developer shell.
-            ornl-slicer2-dev = pkgs.mkShell.override { stdenv = stdenv; } rec {
+            s2-dev = pkgsNative.mkShell.override { stdenv = llvm.stdenv; } rec {
                 name = "s2-dev";
 
                 packages = [
-                    pkgs.cntr
-                    pkgs.doxygen
-                    pkgs.graphviz
-                    pkgs.ccache
+                    pkgsNative.cntr
+
+                    pkgsNative.ccache
+                    pkgsNative.git
+                    pkgsNative.python3
+                    pkgsNative.jq
+                    pkgsNative.moreutils
+
+                    pkgsNative.doxygen
+                    pkgsNative.graphviz
+
+                    pkgsNative.clazy
                     llvm.lldb
-                    pkgs.clazy
-                    ( pkgs.clang-tools.override { llvmPackages = llvm; stdenv = stdenv; } )
-                ] ++ self.outputs.packages.${system}.ornl-slicer2.buildInputs;
+                    llvm.clang-tools
+                ] ++ self.outputs.packages.${system}.ornl.slicer2.buildInputs
+                  ++ self.outputs.packages.${system}.ornl.slicer2.nativeBuildInputs;
 
                 nativeBuildInputs = [
-                    pkgs.qt6.wrapQtAppsHook
-                    pkgs.makeWrapper
-                    pkgs.openssl
+                    pkgsNative.qt6.wrapQtAppsHook
+                    pkgsNative.makeWrapper
+                    pkgsNative.openssl
                 ];
 
                 # For dev, we want to disable hardening.
@@ -159,6 +188,23 @@
                 ];
 
                 shellHook = ''
+                    # Some util variables
+                    export FLAKE_ROOT=$(git rev-parse --show-toplevel)
+
+                    # Utility function to update tools in the cmake preset, called manually.
+                    function manualUpdatePreset() {
+                        export NIX_CMAKE_PRESET_FILE="$FLAKE_ROOT/cmake/presets/nix.json"
+                        jq --indent 4 '.configurePresets[0].cmakeExecutable                   = "${pkgsNative.cmake}/bin/cmake"' "$NIX_CMAKE_PRESET_FILE" | sponge "$NIX_CMAKE_PRESET_FILE"
+                        jq --indent 4 '.configurePresets[0].cacheVariables.CMAKE_C_COMPILER   = "${llvm.clang}/bin/clang"'       "$NIX_CMAKE_PRESET_FILE" | sponge "$NIX_CMAKE_PRESET_FILE"
+                        jq --indent 4 '.configurePresets[0].cacheVariables.CMAKE_CXX_COMPILER = "${llvm.clang}/bin/clang++"'     "$NIX_CMAKE_PRESET_FILE" | sponge "$NIX_CMAKE_PRESET_FILE"
+                        jq --indent 4 '.configurePresets[0].cacheVariables.CMAKE_MAKE_PROGRAM = "${pkgsNative.ninja}/bin/ninja"' "$NIX_CMAKE_PRESET_FILE" | sponge "$NIX_CMAKE_PRESET_FILE"
+
+                        jq --indent 4 '.configurePresets[0].vendor."qt.io/QtCreator/1.0".debugger.Binary  = "${llvm.lldb}/bin/lldb"' "$NIX_CMAKE_PRESET_FILE" | sponge "$NIX_CMAKE_PRESET_FILE"
+                        jq --indent 4 '.configurePresets[0].vendor."qt.io/QtCreator/1.0".debugger.Version = "${llvm.lldb.version}"'  "$NIX_CMAKE_PRESET_FILE" | sponge "$NIX_CMAKE_PRESET_FILE"
+
+                        echo "Updated cmake preset."
+                    }
+
                     # Bit of a weird one - Qt programs can't find plugin path unwrapped, so we make a temporary wrapper and source its env.
                     # See: https://discourse.nixos.org/t/python-qt-woes/11808/10
                     setQtEnvironment=$(mktemp)
@@ -166,8 +212,17 @@
                     makeWrapper "$(type -p sh)" "$setQtEnvironment" "''${qtWrapperArgs[@]}" --argv0 "$random"
                     sed "/$random/d" -i "$setQtEnvironment"
                     source "$setQtEnvironment"
+
+                    # Make sure that git hooks are enabled.
+                    git config core.hooksPath "$FLAKE_ROOT/.gitsetup/hooks"
+
+                    # Allow software to find OpenGL drivers.
+                    export LD_FALLBACK_PATH=/usr/lib/x86_64-linux-gnu
+
+                    # Lastly, let the XDG_CONFIG_HOME be set to the flake root.
+                    export XDG_CONFIG_HOME="$FLAKE_ROOT/.xdg_config"
                 '';
             };
         };
-    };
+    });
 }
