7d3bdc8f3a51db82f21c6dfc26e2de3dd2b5768b
Liam White <liamnwhite1@gmail.com>
2025-02-05 09:58:39 -0500

    perf: improve performance and documentation of GCodeSimulationOutput class

--- include/threading/gcode_simulation_output.h
+++ include/threading/gcode_simulation_output.h
@@ -1,14 +1,13 @@
 #ifndef GCODE_SIMULATION_OUTPUT_H
 #define GCODE_SIMULATION_OUTPUT_H
 
+// Local
+#include "gcode/gcode_meta.h"
+
 // Qt
 #include <QThread>
 
-#include "gcode/gcode_meta.h"
-#include "utilities/mathutils.h"
-
-namespace ORNL
-{
+namespace ORNL {
 /*!
      * \class GCodeSimulationOutput
      * \brief Threaded class that provides additional gcode processing for user with ABAQUS.
@@ -17,39 +16,47 @@ class GCodeSimulationOutput : public QThread {
     Q_OBJECT
 public:
     //! \brief Constructor
-    //! \param tempLocation: location of gcode file
-    //! \param path: path to output
-    //! \param filename: filename to output
-    //! \param text: current gcode
-    //! \param meta: meta used to generate gcode
-    GCodeSimulationOutput(QString tempLocation, QString path, QString filename, QString text, GcodeMeta meta);
+    //! \param temp_location location of gcode file
+    //! \param path path to output
+    //! \param filename filename to output
+    //! \param text current gcode
+    //! \param meta meta used to generate gcode
+    GCodeSimulationOutput(const QString& temp_location,
+                          const QString& path,
+                          const QString& filename,
+                          const QString& text,
+                          const GcodeMeta& meta);
 
     //! \brief Function that is run when start is called on this thread.
     void run() override;
 
     //! \brief Calculate the time needed for the current move
-    //! \param X: the new X position
-    //! \param Y: the new Y position
-    //! \param Z: the new Z position
-    //! \param W: the new W position
-    //! \param F: the new velocity command
-    void calculateTime(QString X, QString Y, QString Z, QString W, QString F);
+    //! \param x the new X position
+    //! \param y the new Y position
+    //! \param z the new Z position
+    //! \param w the new W position
+    //! \param f the new velocity command
+    void calculateTime(const QString& x,
+                       const QString& y,
+                       const QString& z,
+                       const QString& w,
+                       const QString& f);
 
 private:
     //! \brief Temporary file location, output path, output filename, and text to output
     QString m_temp_location, m_path, m_filename, m_text;
 
-    //! \brief Current X, Y, Z, and W locations
-    Distance currentX, currentY, currentZ, currentW;
+    //! \brief Current x, y, z, and w locations
+    Distance m_current_x, m_current_y, m_current_z, m_current_w;
 
     //! \brief Defines whether the syntax uses metric units
-    bool useMetric;
+    bool m_use_metric;
 
     //! \brief Defines the move as G0 and not requiring an F
-    bool isG0;
+    bool m_is_g0;
 
     //! \brief Current elapsed time
-    Time currentTime;
+    Time m_current_time;
 
     //! \brief Meta info determined from file
     GcodeMeta m_selected_meta;
